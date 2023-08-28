b9ac7408b4692738628ed0f5f795985c42c35f48
aniketalok863 <aniketalok863@gmail.com>
2023-08-28 11:52:19 +0530

    Connected Db for insert and fetch history

--- public/script.js
+++ public/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('conversion-form');
     const resultElement = document.getElementById('result');
     const resetButton = document.getElementById('reset-button');
+    const historyButton = document.getElementById('history-button');
+    const historyTableBody = document.getElementById('history-table-body');
 
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
@@ -14,13 +16,59 @@ document.addEventListener('DOMContentLoaded', () => {
         const data = await response.json();
 
         resultElement.innerHTML = `Converted Amount: ${data.convertedAmount.toFixed(2)} ${targetCurrency}`;
-    }
-    );
+
+        // After successful conversion, log the conversion
+        logConversion(sourceCurrency, amount, targetCurrency, data.convertedAmount);
+    });
+
+    historyButton.addEventListener('click', () => {
+        fetchConversionHistory();
+    });
+
     resetButton.addEventListener('click', () => {
         resetForm();
     });
+
     function resetForm() {
         form.reset(); // Clear form fields
         resultElement.innerHTML = ''; // Clear the result display
+    }
+
+    async function logConversion(sourceCurrency, sourceAmount, targetCurrency, convertedAmount) {
+        const conversionData = {
+            sourceCurrency,
+            sourceAmount,
+            targetCurrency,
+            convertedAmount,
+        };
+
+        await fetch('/logConversion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(conversionData),
+        });
+    }
+
+    async function fetchConversionHistory() {
+        const response = await fetch('/getHistory');
+        const historyData = await response.json();
+
+        // Clear previous history
+        historyTableBody.innerHTML = '';
+
+        // Display conversion history in the table
+        historyData.forEach(log => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${log.date}</td>
+                <td>${log.source_currency}</td>
+                <td>${log.source_amount}</td>
+                <td>${log.target_currency}</td>
+                <td>${log.converted_amount}</td>
+            `;
+            historyTableBody.appendChild(row);
+        });
     }
 });
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg'); // Import PostgreSQL Pool
 const app = express();
 
 app.use(cors()); // Enable CORS
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/convert', (req, res) => {
+// Create a PostgreSQL connection pool
+const pool = new Pool({
+    user: 'aniketalok',       // Replace with your PostgreSQL username
+    host: 'localhost',           // Replace with your PostgreSQL host
+    database: 'currency_converter_db',   // Replace with your PostgreSQL database name
+    password: 'root',   // Replace with your PostgreSQL password
+    port: 5432,                  // Default PostgreSQL port
+});
+
+app.get('/convert', async (req, res) => {
     const sourceCurrency = req.query.source;
     const targetCurrency = req.query.target;
     const amount = parseFloat(req.query.amount);
@@ -25,6 +35,33 @@ app.get('/convert', (req, res) => {
     const convertedAmount = amount * rate;
 
     res.json({ convertedAmount });
+});
+
+// Log conversion endpoint
+app.post('/logConversion', express.json(), async (req, res) => {
+    const { sourceCurrency, sourceAmount, targetCurrency, convertedAmount } = req.body;
+
+    try {
+        const query = 'INSERT INTO conversion_logs (date, source_currency, source_amount, target_currency, converted_amount) VALUES (NOW(), $1, $2, $3, $4)';
+        await pool.query(query, [sourceCurrency, sourceAmount, targetCurrency, convertedAmount]);
+        res.status(201).json({ message: 'Conversion logged successfully' });
+    } catch (error) {
+        console.error('Error logging conversion:', error);
+        res.status(500).json({ message: 'An error occurred while logging the conversion' });
+    }
+});
+
+// Fetch conversion history endpoint
+app.get('/getHistory', async (req, res) => {
+    try {
+        const query = 'SELECT * FROM conversion_logs ORDER BY date DESC';
+        const result = await pool.query(query);
+        const conversionLogs = result.rows;
+        res.json(conversionLogs);
+    } catch (error) {
+        console.error('Error fetching conversion history:', error);
+        res.status(500).json({ message: 'An error occurred while fetching conversion history' });
+    }
 });
 
 app.listen(3000, () => {
